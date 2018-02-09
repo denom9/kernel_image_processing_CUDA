249939a51fa47af35cf6d9d22b0598e3b3ac7fd0
Samuele <denom9@gmail.com>
2018-02-09 17:03:29 +0100

    Variable kernel size, adjusted tile creation

--- src/Kernel Image CUDA.cu
+++ src/Kernel Image CUDA.cu
@@ -17,6 +17,7 @@
 #include <numeric>
 #include <stdlib.h>
 #include <stdio.h>
+//#include <chrono>
 #include "PPM.h"
 #include "kernels.h"
 
@@ -31,6 +32,7 @@ static void CheckCudaErrorAux (const char *, unsigned, const char *, cudaError_t
 __constant__ float deviceKernels[3*3 * RESULTS_NUM];
 
 
+
 __global__ void kernelConvolution(float* img, float** output ,const int imageWidth, const int imageHeight, const int imageChannels){
 	__shared__ float imgTile[INPUT_TILE_WIDTH][INPUT_TILE_WIDTH]; //condivisa a livello di blocco
 
@@ -40,9 +42,11 @@ __global__ void kernelConvolution(float* img, float** output ,const int imageWid
 
 
 	if((row < imageHeight) && (col < imageWidth)){
-		int xIndex;
-		int yIndex;
 		int kIndex;
+		int a,b;
+		int border = KERNEL_SIZE / 2;
+		int rowImgBorder,colImgBorder,rowTileBorder,colTileBorder,rowInputBorder,colInputBorder;
+
 
 		int tileRow = row % TILE_WIDTH;
 		int tileCol = col % TILE_WIDTH;
@@ -52,6 +56,18 @@ __global__ void kernelConvolution(float* img, float** output ,const int imageWid
 		for(int h = 0; h < RESULTS_NUM; h++)
 			sum[h] = 0;
 
+		/* offset per gestire i bordi immagine, bordi tile e bordi dell'input */
+		rowInputBorder = (tileRow == 0)? border : 0;
+		colInputBorder = (tileCol == 0)? border : 0;
+
+		rowTileBorder = (tileRow == TILE_WIDTH - 1)? border : 0;
+		colTileBorder = (tileCol == TILE_WIDTH - 1)? border : 0;
+
+		rowImgBorder = (row == imageHeight - 1)? border : 0;
+		colImgBorder = (col == imageWidth - 1)? border : 0;
+
+
+
 		//printf("row: %i, col: %i,tileCell: %i, tileRow: %i, tileCol: %i\n",row,col,tileCell,tileRow,tileCol);
 		for(int c = 0; c < imageChannels; c++){
 
@@ -59,85 +75,96 @@ __global__ void kernelConvolution(float* img, float** output ,const int imageWid
 			element = img[(row * imageWidth + col) * imageChannels + c];
 
 
+			/*
+			 * devo gestire tre casi principali:
+			 *	- bordo immagine: per i pixel necessari per la convoluzione che "vanno fuori" dall'immagine vengono usati i corrispondenti pixel di bordo
+			 *	- bordo del tile non di bordo immagine: per i pixel necessari alla convoluzione bisogna prendere pixel che apparterrebbero ad altri tile adiacenti
+			 *	- cella non di bordo: ho tutti i pixel necessari alla convoluzione
+			 */
 
+			if((row == 0)||(row == imageHeight-1)||(col == 0)||(col == imageWidth-1)){ //bordi immagine
 
-			//controllo i lati dei tile di bordo
-			if(row == 0){ //bordo immagine superiore
+				if(((row == 0)||(row == imageHeight-1))&&((col == 0)||(col == imageWidth-1))){ //angoli immagine
+					for(a = 0; a < border+1; a++){
+						for(b = 0; b < border+1; b++)
+							imgTile[tileRow+a+rowImgBorder][tileCol+b+colImgBorder]=element;
+					}
+				}
 
-				if((col == 0)||(tileCol == 0))//angolo immagine superiore sinistro
-					imgTile[tileRow][tileCol] = element;
+				else if(((row == 0)||(row == imageHeight-1))&&((tileCol == 0)||(tileCol == TILE_WIDTH-1))){ //angoli intermedi superiori e inferiori sinistri
+					for(a = 0; a < border+1; a++){
+						for(b = 0; b < border+1; b++){
+							if(((a < border)&&(row==0))||((a > 0)&&(row==imageHeight-1)))
+								imgTile[tileRow+a+rowImgBorder][tileCol+b+colTileBorder]=element;
+							else
+								imgTile[tileRow+a+rowImgBorder][tileCol+b+colTileBorder]=img[((row-rowInputBorder+a) * imageWidth + (col-colInputBorder+b)) * imageChannels + c];
+						}
+					}
+				}
 
-				if((col == imageWidth - 1)||(tileCol == TILE_WIDTH - 1))//angolo immagine superiore destro
-					imgTile[tileRow][tileCol+2] = element;
+				else if(((col == 0)||(col == imageWidth-1))&&((tileRow == 0)||(tileRow == TILE_WIDTH-1))){ //angoli intermedi destri e sinistri
+					for(a = 0; a < border+1; a++){
+						for(b = 0; b < border+1; b++){
+							if(((b < border)&&(col==0))||((b > 0)&&(col==imageWidth-1)))
+								imgTile[tileRow+a+rowTileBorder][tileCol+b+colImgBorder]=element;
+							else
+								imgTile[tileRow+a+rowTileBorder][tileCol+b+colImgBorder]=img[((row-rowInputBorder+a) * imageWidth + (col-colInputBorder+b)) * imageChannels + c];
+						}
+					}
+				}
 
-				imgTile[tileRow][tileCol+1] = element;
-			}
-			else if(row == imageHeight - 1){ //bordo immagine inferiore
-
-				if((col == 0)||(tileCol == 0))//angolo immagine inferiore sinistro
-					imgTile[tileRow+2][tileCol] = element;
-
-				if((col == imageWidth - 1)||(tileCol == TILE_WIDTH - 1))//angolo immagine inferiore destro
-					imgTile[tileRow+2][tileCol+2] = element;
-
-				imgTile[tileRow+2][tileCol+1] = element;
-			}
-
-			if(col == 0){//bordo immagine sinistro
-				if(tileRow == 0)
-					imgTile[tileRow][tileCol] = element;
-				if(tileRow == TILE_WIDTH -1)
-					imgTile[tileRow+2][tileCol] = element;
-				imgTile[tileRow+1][tileCol] = element;
-			}
-			else if(col == imageWidth - 1){//bordo immagine destro
-				if(tileRow == 0)
-					imgTile[tileRow][tileCol+2] = element;
-				if(tileRow == TILE_WIDTH -1)
-					imgTile[tileRow+2][tileCol+2] = element;
-				imgTile[tileRow+1][tileCol+2] = element;
-			}
-
-			//controllo i lati dei tile centrali
-			if((tileRow == 0) && (row != 0)){ //lato superiore
-
-				if(tileCol == 0) // angolo superiore sinistro
-					imgTile[tileRow][tileCol]=img[((row-1) * imageWidth + (col-1)) * imageChannels + c];
-
-				if(tileCol == TILE_WIDTH - 1) //angolo superiore destro
-					imgTile[tileRow][tileCol+2]=img[((row-1) * imageWidth + (col+1)) * imageChannels + c];
-
-				imgTile[tileRow][tileCol+1]=img[((row-1) * imageWidth + col) * imageChannels + c];
-			}
-
-
-			if((tileRow == TILE_WIDTH - 1) && (row != imageHeight - 1)){ //lato inferiore
-
-				if((tileCol == 0))//angolo inferiore sinistro
-					imgTile[tileRow+2][tileCol]=img[((row+1) * imageWidth + (col-1)) * imageChannels + c];
-
-				if((tileCol == TILE_WIDTH - 1)) //angolo inferiore destro
-					imgTile[tileRow+2][tileCol+2]=img[((row+1) * imageWidth + (col+1)) * imageChannels + c];
-
-				imgTile[tileRow+2][tileCol+1]=img[((row+1) * imageWidth + col) * imageChannels + c];
+				else{ //lati immagine
+					if((row == 0)||(row == imageHeight-1)){ //lati superiore e inferiore
+						for(a = 0; a < border+1; a++)
+							imgTile[tileRow+a+rowImgBorder][tileCol+border]=element;
+					}
+					else if((col == 0)||(col == imageWidth-1)){ //lati sinistro e destro
+						for(a = 0; a < border+1; a++)
+							imgTile[tileRow+border][tileCol+a+colImgBorder]=element;
+					}
+				}
 			}
 
-			if((tileCol == 0)&&(col > 0)) //lato sinistro
-				imgTile[tileRow+1][tileCol]=img[((row) * imageWidth + (col-1)) * imageChannels + c];
-			if((tileCol == TILE_WIDTH - 1)&&(col < imageWidth)) //lato destro
-				imgTile[tileRow+1][tileCol+2]=img[((row) * imageWidth + (col+1)) * imageChannels + c];
+
+			else if((tileRow == 0)||(tileRow == TILE_WIDTH-1)||(tileCol == 0)||(tileCol == TILE_WIDTH-1)){ //bordi tile interni
+
+				if(((tileRow == 0)||(tileRow == TILE_WIDTH-1))&&((tileCol == 0)||(tileCol == TILE_WIDTH-1))){ //angoli tile interni
+					for(a = 0; a < border+1; a++){
+						for(b = 0; b < border+1; b++)
+							imgTile[tileRow+a+rowTileBorder][tileCol+b+colTileBorder]=img[((row-rowInputBorder+a) * imageWidth + (col-colInputBorder+b)) * imageChannels + c];
+					}
+
+				}
+				else{ //bordi tile interni
+					if((tileRow == 0)||(tileRow == TILE_WIDTH-1)){
+						for(a = 0; a < border+1; a++)
+							imgTile[tileRow+a+rowTileBorder][tileCol+border]=img[((row-rowInputBorder+a) * imageWidth + col) * imageChannels + c];
+					}
+					else if((tileCol == 0)||(tileCol == TILE_WIDTH-1)){
+						for(a = 0; a < border+1; a++)
+							imgTile[tileRow+border][tileCol+a+colTileBorder]=img[(row * imageWidth + (col-colInputBorder+a)) * imageChannels + c];
+					}
+				}
+
+			}
+
+
+			else //cella non di bordo
+				imgTile[tileRow+border][tileCol+border] = element;
 
 
 
-			imgTile[tileRow+1][tileCol+1] = element;
+
+
+
 			__syncthreads();
 
 
 			/*calcolo dell'output*/
 			for(int i = 0; i < KERNEL_SIZE; i++){
-				yIndex = ((row - 1 + i) < 0) ? 0 : ((row - 1 + i) >= imageHeight) ? imageHeight - 1 : row - 1 + i;
+				//yIndex = ((row - 1 + i) < 0) ? 0 : ((row - 1 + i) >= imageHeight) ? imageHeight - 1 : row - 1 + i;
 				for(int j = 0; j < KERNEL_SIZE; j++){
-					xIndex = ((col - 1 + j) < 0) ? 0 : ((col - 1 + j) >= imageWidth) ? imageWidth - 1 : col - 1 + j;
+					//xIndex = ((col - 1 + j) < 0) ? 0 : ((col - 1 + j) >= imageWidth) ? imageWidth - 1 : col - 1 + j;
 					kIndex = (2 - i)*3 + (2 -j);
 
 					for(int h = 0; h < RESULTS_NUM; h++)
@@ -173,6 +200,8 @@ int main(int argc,  char** argv){
     float *hostOutput[RESULTS_NUM];
     float *hostKernels[RESULTS_NUM];
 
+
+
     hostInput = Image_getData(inputImage);
 
     //creo le immagini di output
@@ -206,7 +235,10 @@ int main(int argc,  char** argv){
 
     dim3 gridDim(ceil((float)imageWidth / TILE_WIDTH),ceil((float)imageHeight / TILE_WIDTH));
     dim3 blockDim(TILE_WIDTH,TILE_WIDTH);
-    printf("%i,%i\n",(imageWidth / TILE_WIDTH) + 1,(imageHeight / TILE_WIDTH) + 1);
+    //printf("%i,%i\n",(imageWidth / TILE_WIDTH) + 1,(imageHeight / TILE_WIDTH) + 1);
+
+    //auto start = std::chrono::system_clock::now();
+
     kernelConvolution<<<gridDim,blockDim>>>(deviceInput,deviceOutput,imageWidth,imageHeight,imageChannels);
 
     cudaDeviceSynchronize();
@@ -219,13 +251,13 @@ int main(int argc,  char** argv){
 
 
     //esporto le immagini
-	PPM_export("processed/identity.ppm",outputImages[0]);
-    PPM_export("processed/edge1.ppm",outputImages[1]);
-    PPM_export("processed/edge2.ppm",outputImages[2]);
-    PPM_export("processed/edge3.ppm",outputImages[3]);
-    PPM_export("processed/sharpen.ppm",outputImages[4]);
+	//PPM_export("processed/identity.ppm",outputImages[0]);
+    //PPM_export("processed/edge1.ppm",outputImages[1]);
+   // PPM_export("processed/edge2.ppm",outputImages[2]);
+    //PPM_export("processed/edge3.ppm",outputImages[3]);
+    //PPM_export("processed/sharpen.ppm",outputImages[4]);
     PPM_export("processed/boxblur.ppm",outputImages[5]);
-    PPM_export("processed/gaussianblur3.ppm",outputImages[6]);
+    //PPM_export("processed/gaussianblur3.ppm",outputImages[6]);
 
     //libero la memoria
     CUDA_CHECK_RETURN(cudaFree(deviceInput));
@@ -237,109 +269,12 @@ int main(int argc,  char** argv){
 
 
 
+    //auto end = std::chrono::system_clock::now();
+    //std::chrono::duration<double> elapsed = end-start;
+
+    //printf("Elapsed time:%d\n",elapsed);
 
 
-
-
-    /*
-	float *hostIdentityOutput;
-	float *hostEdge1Output;
-	float *hostEdge2Output;
-	float *hostEdge3Output;
-	float *hostSharpenOutput;
-	float *hostBoxblurOutput;
-	float *hostGaussianblur3Output;
-
-	float *deviceIdentityOutput;
-	float *deviceEdge1Output;
-	float *deviceEdge2Output;
-	float *deviceEdge3Output;
-	float *deviceSharpenOutput;
-	float *deviceBoxblurOutput;
-	float *deviceGaussianblur3Output;
-
-	Image_t* identityOutput = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* edge1Output = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* edge2Output = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* edge3Output = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* sharpenOutput = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* boxblurOutput = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* gaussianblur3Output = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* gaussianblur5Output = Image_new(imageWidth,imageHeight,imageChannels);
-	Image_t* unsharpOutput = Image_new(imageWidth,imageHeight,imageChannels);
-
-	hostIdentityOutput = Image_getData(identityOutput);
-	hostEdge1Output = Image_getData(edge1Output);
-	hostEdge2Output = Image_getData(edge2Output);
-	hostEdge3Output = Image_getData(edge3Output);
-	hostSharpenOutput = Image_getData(sharpenOutput);
-	hostBoxblurOutput = Image_getData(boxblurOutput);
-	hostGaussianblur3Output = Image_getData(gaussianblur3Output);
-
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceIdentity,sizeof(float)*3*3));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceEdge1,sizeof(float)*3*3));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceEdge2,sizeof(float)*3*3));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceEdge3,sizeof(float)*3*3));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceSharpen,sizeof(float)*3*3));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceBoxblur,sizeof(float)*3*3));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceGaussianblur3,sizeof(float)*3*3));
-
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceIdentityOutput,imageDataSize));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceEdge1Output,imageDataSize));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceEdge2Output,imageDataSize));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceEdge3Output,imageDataSize));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceSharpenOutput,imageDataSize));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceBoxblurOutput,imageDataSize));
-    CUDA_CHECK_RETURN(cudaMalloc((void**)&deviceGaussianblur3Output,imageDataSize));
-
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceIdentityOutput,hostIdentityOutput,imageDataSize,cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceEdge1Output,hostEdge1Output,imageDataSize,cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceEdge2Output,hostEdge2Output,imageDataSize,cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceEdge3Output,hostEdge3Output,imageDataSize,cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceSharpenOutput,hostSharpenOutput,imageDataSize,cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceBoxblurOutput,hostBoxblurOutput,imageDataSize,cudaMemcpyHostToDevice));
-    CUDA_CHECK_RETURN(cudaMemcpy(deviceGaussianblur3Output,hostGaussianblur3Output,imageDataSize,cudaMemcpyHostToDevice));
-
-    CUDA_CHECK_RETURN(cudaMemcpy(hostIdentityOutput,deviceIdentityOutput,imageDataSize,cudaMemcpyDeviceToHost));
-    CUDA_CHECK_RETURN(cudaMemcpy(hostEdge1Output,deviceEdge1Output,imageDataSize,cudaMemcpyDeviceToHost));
-    CUDA_CHECK_RETURN(cudaMemcpy(hostEdge2Output,deviceEdge2Output,imageDataSize,cudaMemcpyDeviceToHost));
-    CUDA_CHECK_RETURN(cudaMemcpy(hostEdge3Output,deviceEdge3Output,imageDataSize,cudaMemcpyDeviceToHost));
-    CUDA_CHECK_RETURN(cudaMemcpy(hostSharpenOutput,deviceSharpenOutput,imageDataSize,cudaMemcpyDeviceToHost));
-    CUDA_CHECK_RETURN(cudaMemcpy(hostBoxblurOutput,deviceBoxblurOutput,imageDataSize,cudaMemcpyDeviceToHost));
-    CUDA_CHECK_RETURN(cudaMemcpy(hostGaussianblur3Output,deviceGaussianblur3Output,imageDataSize,cudaMemcpyDeviceToHost));
-
-    Image_setData(identityOutput,hostIdentityOutput);
-    Image_setData(edge1Output,hostEdge1Output);
-    Image_setData(edge2Output,hostEdge2Output);
-    Image_setData(edge3Output,hostEdge3Output);
-    Image_setData(sharpenOutput,hostSharpenOutput);
-    Image_setData(boxblurOutput,hostBoxblurOutput);
-    Image_setData(gaussianblur3Output,hostGaussianblur3Output);
-
-   	PPM_export("processed/identity.ppm",identityOutput);
-    PPM_export("processed/edge1.ppm",edge1Output);
-    PPM_export("processed/edge2.ppm",edge2Output);
-    PPM_export("processed/edge3.ppm",edge3Output);
-    PPM_export("processed/sharpen.ppm",sharpenOutput);
-    PPM_export("processed/boxblur.ppm",boxblurOutput);
-    PPM_export("processed/gaussianblur3.ppm",gaussianblur3Output);
-
-    CUDA_CHECK_RETURN(cudaFree(deviceIdentity));
-    CUDA_CHECK_RETURN(cudaFree(deviceEdge1));
-    CUDA_CHECK_RETURN(cudaFree(deviceEdge2));
-    CUDA_CHECK_RETURN(cudaFree(deviceEdge3));
-    CUDA_CHECK_RETURN(cudaFree(deviceSharpen));
-    CUDA_CHECK_RETURN(cudaFree(deviceBoxblur));
-    CUDA_CHECK_RETURN(cudaFree(deviceGaussianblur3));
-
-    CUDA_CHECK_RETURN(cudaFree(deviceIdentityOutput));
-    CUDA_CHECK_RETURN(cudaFree(deviceEdge1Output));
-    CUDA_CHECK_RETURN(cudaFree(deviceEdge2Output));
-    CUDA_CHECK_RETURN(cudaFree(deviceEdge3Output));
-    CUDA_CHECK_RETURN(cudaFree(deviceSharpenOutput));
-    CUDA_CHECK_RETURN(cudaFree(deviceBoxblurOutput));
-    CUDA_CHECK_RETURN(cudaFree(deviceGaussianblur3Output));
-*/
 
 
 
